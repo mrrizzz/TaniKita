3ab7d931cd291d7ac4bb4f4aa2d4983a956010ef
LalaSalwa <lalasalwactk@gmail.com>
2024-09-30 22:58:52 +0700

    revisi

--- components/navbar/auth-button.tsx
+++ components/navbar/auth-button.tsx
@@ -7,10 +7,10 @@ export default function AuthLinks() {
   return (
     <div className="flex items-center space-x-3 rtl:space-x-reverse">
       <Button variant="outline" asChild>
-        <Link href="/signup">Sign Up</Link>
+        <Link href="../core-features/home">Sign Up</Link>
       </Button>
       <Button variant="default" asChild>
-        <Link href="/login">Login</Link>
+        <Link href="../core-features/home">Login</Link>
       </Button>
     </div>
   );
